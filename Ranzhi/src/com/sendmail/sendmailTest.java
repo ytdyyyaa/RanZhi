b8caed336287a40d1007bd739b7f5340817c6eb9
ytdyyaa <1497277185@qq.com>
2017-12-13 17:02:59 +0800

    修改了邮件的内容

--- Ranzhi/src/com/sendmail/sendmailTest.java
+++ Ranzhi/src/com/sendmail/sendmailTest.java
@@ -4,23 +4,22 @@ import javax.mail.MessagingException;
 
 public class sendmailTest {
 	Mail mail = null;
-	public void sendMail(String title, String content,String recive) throws MessagingException {
+
+	public void sendMail(String title, String content, String recive) throws MessagingException {
 		mail = new Mail();
-	
+
 		mail.setMessage(title, content);
-		mail.addAttachment
-		("¸½¼þ", 
-				"F:\\da3shang\\shixun\\Ranzhi\\zip\\zip.zip");
-			
+		mail.addAttachment("¸½¼þ", "F:\\da3shang\\shixun\\Ranzhi\\zip\\zip.zip");
+
 		mail.addToRecipent(recive);
-		
+
 		mail.sendMail("dycn993@126.com", "superjuniorlj1", "jijinwen");
-		
+
 	}
-	
-	public static void main(String args[]) throws MessagingException{
+
+	public static void main(String args[]) throws MessagingException {
 		sendmailTest mail = new sendmailTest();
-		mail.sendMail("report","success","1497277185@qq.com");
-		
+		mail.sendMail("The test report from jijinwen ", "The test cases were completed", "626231936@qq.com");
+
 	}
 }
